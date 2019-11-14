b0c6721f6ac6d6bb52c2a08e234394bd2b336c0e
IsaacVerm <isaacverm@gmail.com>
2019-11-14 12:13:37 +0100

    add get-data-sporza iteration functions

--- R/get-data-sporza.R
+++ R/get-data-sporza.R
@@ -1,4 +1,4 @@
-get_matchdays <- function(season_id, gameweek_id) {
+get_matchdays <- function(gameweek_id, season_id) {
   url <- paste0('https://api.sporza.be/web/soccer/matchdays/',
                 season_id,
                 '/',
@@ -8,8 +8,18 @@ get_matchdays <- function(season_id, gameweek_id) {
   return(response_body)
 }
 
+get_all_matchdays <- function(gameweek_ids, season_id) {
+  gameweek_ids %>% purrr::map(get_matchdays, season_id)
+}
+
 parse_games <- function(matchdays_response) {
   return(matchdays_response$groupedMatches[[1]][["matches"]])
+}
+
+parse_all_games <- function(matchdays_responses) {
+  matchdays_responses %>%
+    purrr::map(parse_games) %>%
+    purrr::flatten()
 }
 
 parse_game_result <- function(game) {
@@ -19,4 +29,7 @@ parse_game_result <- function(game) {
              away_score = game$awayScore)
 }
 
-
+parse_all_game_results <- function(games) {
+  games %>%
+    purrr::map(parse_game_result)
+}
