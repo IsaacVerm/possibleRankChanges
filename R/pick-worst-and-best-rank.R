7d912366067bc5220c61453432faac9534b5b3cd
IsaacVerm <isaacverm@gmail.com>
2019-11-14 20:48:27 +0100

    add pick-worst-and-best-rank functions

---
+++ R/pick-worst-and-best-rank.R
@@ -0,0 +1,18 @@
+biggest_drop_in_rank <- function(possible_league_tables) {
+  possible_league_tables %>%
+    dplyr::group_by(team) %>%
+    dplyr::summarise(biggest_drop_in_rank = min(rank_difference))
+}
+
+biggest_rank_increase <- function(possible_league_tables) {
+  possible_league_tables %>%
+    dplyr::group_by(team) %>%
+    dplyr::summarise(biggest_rank_increase = max(rank_difference))
+}
+
+create_rank_table <- function(league_table, possible_league_tables) {
+  league_table %>%
+    dplyr::select(team, rank) %>%
+    dplyr::left_join(biggest_drop_in_rank(possible_league_tables), by = "team") %>%
+    dplyr::left_join(biggest_rank_increase(possible_league_tables), by = "team")
+}
