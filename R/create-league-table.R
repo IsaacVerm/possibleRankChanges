fcf5d19f581747c49976f34791fa249147c1b20d
IsaacVerm <isaacverm@gmail.com>
2019-11-14 16:18:37 +0100

    add create-league-table functions

--- R/create-league-table.R
+++ R/create-league-table.R
@@ -1,3 +1,24 @@
-game_results_to_df <- function(game_results) {
-  dplyr::bind_rows(game_results)
+games_to_df <- function(list_games) {
+  dplyr::bind_rows(list_games)
 }
+
+create_home_league_table <- function(game_results) {
+  game_results %>%
+    dplyr::group_by(home_team) %>%
+    dplyr::summarise(total_home_points = sum(home_points))
+}
+
+create_away_league_table <- function(game_results) {
+  game_results %>%
+    dplyr::group_by(away_team) %>%
+    dplyr::summarise(total_away_points = sum(away_points))
+}
+
+create_league_table <- function(home_league_table, away_league_table) {
+  home_league_table %>%
+    dplyr::left_join(away_league_table, by = c("home_team" = "away_team")) %>%
+    dplyr::rename(team = home_team) %>%
+    dplyr::mutate(total_points = total_home_points + total_away_points)
+}
+
+
